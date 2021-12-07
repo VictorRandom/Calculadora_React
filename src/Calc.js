ad66279840f5d492d655c3a2ebed89d6968741fe
Victorrandom <victormbalduino@hotmail.com>
2021-12-07 12:15:35 -0300

    operações da calculadora ( adição, subtração, divisão e multiplicação)

--- src/Calc.js
+++ src/Calc.js
@@ -1,41 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Calc.css';
 
 function Calc(){
 
+    const [result, setResult] = useState("");
+
+    function click(e){
+        setResult(result.concat(e.target.value));
+        console.log(e.target.value);
+    };
+
+    function clear(e){
+        setResult("");
+    };
+
+    function erase(e){
+        setResult(result.substring(0, result.length - 1));
+    };
+
+    function equal(e){
+        try{
+        setResult(eval(result).toString())
+        } 
+        catch(e){
+            setResult("ERROR");
+        }
+    };
+
     return(
     <div className="container">
         <h1>Calculadora React</h1>
-        <p>Resultado</p>
+        <div>
+            <input type="text" value={result}></input>
+        </div>
         <div>
             <table className="table">
                 <tr>
-                    <td><button></button>AC</td>
-                    <td><button></button>ER</td>
-                    <td><button></button>x</td>
-                    <td><button></button>/</td>
+                    <td><button onClick={clear}>AC</button></td>
+                    <td><button onClick={erase}>ER</button></td>
+                    <td><button value="*" onClick={click}>x</button></td>
+                    <td><button value="/" onClick={click}>/</button></td>
                 </tr>
                 <tr>
-                    <td><button></button>7</td>
-                    <td><button></button>8</td>
-                    <td><button></button>9</td>
-                    <td><button></button>-</td>
+                    <td><button value="7" onClick={click}>7</button></td>
+                    <td><button value="8" onClick={click}>8</button></td>
+                    <td><button value="9" onClick={click}>9</button></td>
+                    <td><button value="-" onClick={click}>-</button></td>
                 </tr>
                 <tr>
-                    <td><button></button>4</td>
-                    <td><button></button>5</td>
-                    <td><button></button>6</td>
-                    <td><button></button>+</td>
+                    <td><button value="4" onClick={click}>4</button></td>
+                    <td><button value="5" onClick={click}>5</button></td>
+                    <td><button value="6" onClick={click}>6</button></td>
+                    <td><button value="+" onClick={click}>+</button></td>
                 </tr>
                 <tr>
-                    <td><button></button>1</td>
-                    <td><button></button>2</td>
-                    <td><button></button>3</td>
-                    <td><button></button>=</td>
+                    <td><button value="1" onClick={click}>1</button></td>
+                    <td><button value="2" onClick={click}>2</button></td>
+                    <td><button value="3" onClick={click}>3</button></td>
+                    <td><button onClick={equal}>=</button></td>
                 </tr>
                 <tr>
-                    <td><button></button>0</td>
-                    <td><button></button>.</td>
+                    <td><button value="0" onClick={click}>0</button></td>
+                    <td><button value="." onClick={click}>.</button></td>
                 </tr>
             </table>
 
